817f73ac1afb601247affc11aa1e25bba5732098
Uíza <uizacarvalhosilva@gmail.com>
2026-01-14 12:00:35 -0300

    Initial Commit

---
+++ Follow/FollowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FollowApp.swift
+//  Follow
+//
+//  Created by Uíza Carvalho on 14/01/26.
+//
+
+import SwiftUI
+
+@main
+struct FollowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
